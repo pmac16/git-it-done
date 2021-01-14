6e17cd63360cb989cbd980542385c6dab76c6b21
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-01-13 20:53:37 -0800

    updated request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    //format the github api url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
 
-getUserRepos();
+    //make a request to the URL
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data)
+    });
+});
+}
+getUserRepos("pmac16");
